22ed1ffae90d5abe80bd14cdc32d5cf070106201
AbiMA <48912566+indigosaurus@users.noreply.github.com>
2023-03-20 02:01:29 +0000

    Final Submission.

--- Bing/Bing/BingMap.ts
+++
@@ -1,9 +0,0 @@
-var navigationBarMode = Microsoft.Maps.NavigationBarMode;
-var map = new Microsoft.Maps.Map(
-    document.getElementById('myMap'),
-    {
-        /* No need to set credentials if already passed in URL */
-        navigationBarMode: navigationBarMode.compact,
-        supportedMapTypes: [Microsoft.Maps.MapTypeId.road, Microsoft.Maps.MapTypeId.aerial, Microsoft.Maps.MapTypeId.grayscale, Microsoft.Maps.MapTypeId.canvasLight]
-    }
-);
